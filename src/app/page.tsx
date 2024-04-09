d7e64ee6dc5036cd0b246feb59559b0bfe22b782
OSE-ICE <ose1975@gmail.com>
2024-04-09 09:25:52 +0000

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,88 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+import "./globals.css";
+
+const GameReview = ({
+  name,
+  desc,
+  review,
+}: {
+  name: string;
+  desc: string;
+  review: number;
+}) => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div className="review-container">
+        <div>Name: {name}</div>
+        <div>Description: {desc}</div>
+        <div>Review: {review}</div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
-}
+};
+
+export default () => {
+  const [gameReviews, setGameReviews] = useState([
+    { name: "fifa", desc: "football game", review: 10 },
+  ]);
+  const [showReview, setShowReview] = useState(false);
+  const [newReview, setNewReview] = useState({
+    name: "",
+    desc: "",
+    review: 0,
+  });
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    field: string
+  ) => {
+    setNewReview({ ...newReview, [field]: e.target.value });
+  };
+
+  const handleAddReview = () => {
+    setGameReviews([...gameReviews, newReview]);
+    setNewReview({ name: "", desc: "", review: 0 });
+    setShowReview(false);
+  };
+
+  return (
+    <>
+      {gameReviews.map((review, index) => (
+        <GameReview key={index} {...review} />
+      ))}
+      <div>
+        <button className="review-button" onClick={() => setShowReview(true)}>
+          New review
+        </button>
+      </div>
+      {showReview ? (
+        <>
+          <div>
+            Name:{" "}
+            <input
+              className="review-input"
+              onChange={(e) => handleInputChange(e, "name")}
+            ></input>
+          </div>
+          <div>
+            Desc:{" "}
+            <input
+              className="review-input"
+              onChange={(e) => handleInputChange(e, "desc")}
+            ></input>
+          </div>
+          <div>
+            Score:{" "}
+            <input
+              className="review-input"
+              onChange={(e) => handleInputChange(e, "review")}
+            ></input>
+          </div>
+          <button onClick={handleAddReview}>Add Review</button>
+        </>
+      ) : null}
+    </>
+  );
+};
